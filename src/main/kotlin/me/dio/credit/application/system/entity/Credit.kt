385e1ecb36f1d494458311ea3c6f287c4d7ebda0
Iago Gonçalves de Meira <iagomeiraifpr@gmail.com>
2024-01-18 11:05:57 -0300

    Terminando todas as modificações do 2º módulo

--- src/main/kotlin/me/dio/credit/application/system/entity/Credit.kt
+++ src/main/kotlin/me/dio/credit/application/system/entity/Credit.kt
@@ -1,16 +1,24 @@
 package me.dio.credit.application.system.entity
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
 import me.dio.credit.application.system.enummeration.Status
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.UUID
 
+@Entity
 data class Credit (
-    val id: Long? = null,
-    val creditCode: UUID = UUID.randomUUID(),
-    val creditValue: BigDecimal = BigDecimal.ZERO,
-    val dayFirstInstallment: LocalDate,
-    val numberOfInstallments: Int = 0,
-    val status: Status = Status.IN_PROGRESS,
-    val costumer: Customer? = null
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null,
+    @Column(nullable = false, unique = true) val creditCode: UUID = UUID.randomUUID(),
+    @Column(nullable = false) val creditValue: BigDecimal = BigDecimal.ZERO,
+    @Column(nullable = false) val dayFirstInstallment: LocalDate,
+    @Column(nullable = false) val numberOfInstallments: Int = 0,
+    @Enumerated val status: Status = Status.IN_PROGRESS,
+    @ManyToOne val customer: Customer? = null
 )
